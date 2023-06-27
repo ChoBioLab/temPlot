6ef461aa4230831289122cacb33caffb30243536
nehamaskey <137223320+nehamaskey@users.noreply.github.com>
2023-06-27 16:13:22 -0400

    Update sandbox.R

--- sandbox.R
+++ sandbox.R
@@ -2,7 +2,10 @@
 
 #scatterplot
 a <- mtcars
-ggplot(a, aes(cyl,mpg, color= cyl))+ geom_point(size = 2) +  xlab("mpg for diff cars") + ylab("cyl for diff cars") +
+ggplot(a, aes(cyl,mpg, color= cyl))+ 
+    geom_point(size = 2) +  
+    xlab("mpg for diff cars") + 
+    ylab("cyl for diff cars") +
     theme_light() +
     theme(legend.position="none", legend.title = element_blank()) +
     theme(axis.text = element_text(colour = "black", family = "Arial", size = 14), axis.title = element_text(size = 14))
@@ -15,9 +18,9 @@ heatmap(mtcarsdata, scale="column")
 #boxplot
 a <- mtcars
 ggplot(a, aes(x=cyl, y=mpg, color= cyl))+
-geom_boxplot()+
-xlab("cyl")+
-ylab("mpg")
+    geom_boxplot()+
+    xlab("cyl")+
+    ylab("mpg")
 
 #how do i make a dotplot with this...looked at Rachel's code from figure 2F
 do_DotPlot(mtcars,
